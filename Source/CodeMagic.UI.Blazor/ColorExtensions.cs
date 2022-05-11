39de35a401217f9ed60881d980367eca4bee3e14
Gvin <gvin.sim@gmail.com>
2022-05-11 16:57:07 +0300

    Fixed SymbolsImageDisplay

---
+++ Source/CodeMagic.UI.Blazor/ColorExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Drawing;
+
+namespace CodeMagic.UI.Blazor;
+
+public static class ColorExtensions
+{
+    private const string DefaultColor = "white";
+
+    public static string ToStyleString(this Color color)
+    {
+        return $"rgba({color.R},{color.G},{color.B},{color.A})";
+    }
+
+    public static string ToStyleString(this Color? color)
+    {
+        if (color == null)
+        {
+            return DefaultColor;
+        }
+
+        return color.Value.ToStyleString();
+    }
+}
